427ce2f62815a4dedb4bf1cc433905fb93460ae9
bmalinowsky <bmalinowsky@github.com>
2015-04-17 16:12:46 +0200

    Quick check if KNX property definitions are available

--- src/main/java/tuwien/auto/calimero/device/BaseKnxDevice.java
+++ src/main/java/tuwien/auto/calimero/device/BaseKnxDevice.java
@@ -42,6 +42,7 @@ import java.util.StringTokenizer;
 
 import tuwien.auto.calimero.IndividualAddress;
 import tuwien.auto.calimero.Settings;
+import tuwien.auto.calimero.exception.KNXException;
 import tuwien.auto.calimero.link.KNXLinkClosedException;
 import tuwien.auto.calimero.link.KNXNetworkLink;
 import tuwien.auto.calimero.log.LogManager;
@@ -82,6 +83,8 @@ public class BaseKnxDevice implements KnxDevice
 
 	private ProcessServiceNotifier procNotifier;
 	private ManagementServiceNotifier mgmtNotifier;
+
+	private static final String propDefinitionsResource = "properties.xml";
 
 	private InterfaceObjectServer ios = null;
 	// The object instance determines which instance of an object type is
@@ -324,6 +327,14 @@ public class BaseKnxDevice implements KnxDevice
 	{
 		if (ios == null) {
 			ios = new InterfaceObjectServer(false);
+			// check property definitions for encoding support before we init basic properties
+			try {
+				ios.loadDefinitions(propDefinitionsResource);
+			}
+			catch (final KNXException e) {
+				// using the default resource ID, we cannot expect to always find the resource
+				logger.info("please load the Interface Object Server KNX property definitions");
+			}
 		}
 
 		// initialize interface device object properties
