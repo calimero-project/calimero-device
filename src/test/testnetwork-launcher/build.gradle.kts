83cee208de67557244bd52818db512815e0477c8
bmalinowsky <bmalinowsky@github.com>
2025-03-11 11:10:50 +0100

    Convert to build.gradle.kts

---
+++ src/test/testnetwork-launcher/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	application
+}
+
+group = "io.calimero"
+version = "3.0-SNAPSHOT"
+
+application {
+	mainClass.set("io.calimero.testnetwork.TestNetwork")
+}
+
+tasks.named<JavaExec>("run") {
+	// for attaching to debugger, start with -Ddebug=true
+	if (System.getProperty("debug", "false") == "true") {
+		jvmArgs("-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000")
+	}
+	systemProperties(System.getProperties() as Map<String?, *>)
+	args("server-config.xml")
+	standardInput = System.`in`
+}
+
+repositories {
+	mavenCentral()
+	mavenLocal()
+	maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+	implementation("${group}:calimero-testnetwork:${version}")
+}
