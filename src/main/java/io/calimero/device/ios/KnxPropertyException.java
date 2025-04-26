d270155da27e445337e6ea8bd7b04399d4b9f1a1
bmalinowsky <bmalinowsky@github.com>
2025-04-26 12:47:23 +0200

    Remove serialVersionUID

--- src/main/java/io/calimero/device/ios/KnxPropertyException.java
+++ src/main/java/io/calimero/device/ios/KnxPropertyException.java
@@ -1,6 +1,6 @@
 /*
     Calimero 3 - A library for KNX network access
-    Copyright (c) 2010, 2020 B. Malinowsky
+    Copyright (c) 2010, 2025 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -46,8 +46,6 @@ import io.calimero.cemi.CEMIDevMgmt;
  */
 public class KnxPropertyException extends KnxRuntimeException
 {
-	private static final long serialVersionUID = 1L;
-
 	private final int code;
 
 	/**
--- src/main/java/io/calimero/device/ios/PropertyEvent.java
+++ src/main/java/io/calimero/device/ios/PropertyEvent.java
@@ -1,6 +1,6 @@
 /*
     Calimero 3 - A library for KNX network access
-    Copyright (c) 2010, 2018 B. Malinowsky
+    Copyright (c) 2010, 2025 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -45,8 +45,6 @@ import java.util.EventObject;
  */
 public class PropertyEvent extends EventObject
 {
-	private static final long serialVersionUID = 1L;
-
 	private final InterfaceObject io;
 	private final int pid;
 	private final int start;
