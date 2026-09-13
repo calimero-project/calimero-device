7ed9a6882b25b242e46e6a8449da03629c01c1b5
bmalinowsky <bmalinowsky@github.com>
2026-09-13 12:56:30 +0200

    Upgrade to buildtools.native 1.1.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	`java-library`
 	`maven-publish`
 	signing
-	id("org.graalvm.buildtools.native") version "1.1.2"
+	id("org.graalvm.buildtools.native") version "1.1.12"
 	id("com.github.ben-manes.versions") version "0.62.0"
 	eclipse
 }
